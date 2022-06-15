5b265aed86a3764079d3c6f481f10c5e83ce323b
Jaden Ryan <jaden.kitonga@student.moringaschool.com>
2022-06-15 18:36:50 +0300

    commit message

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+let form = document.querySelector("create-task-form");
+form.addEventListener("submit", (e)=>{
+  e.preventDefault()
+  let value = document.getElementById("new-task-description").value;
+  handleTask(value)
+  form.reset()
+})
 });
